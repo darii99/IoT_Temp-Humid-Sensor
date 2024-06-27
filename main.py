23876c6d0f7638693bcda6fb25cd12934a619d31
Darina Larsen <85901578+darii99@users.noreply.github.com>
2024-06-27 16:47:56 +0200

    Add files via upload
    Added code and config files

---
+++ main.py
@@ -0,0 +1,14 @@
+# main.py -- put your code here!
+
+from machine import Pin
+import utime as time
+from dht import DHT11, InvalidChecksum
+
+while True:
+    time.sleep(5)
+    pin = machine.Pin(28, Pin.OUT, Pin.PULL_DOWN)
+    sensor = DHT11(pin)
+    t  = (sensor.temperature)
+    h = (sensor.humidity)
+    print("Temperature: {}".format(sensor.temperature))
+    print("Humidity: {}".format(sensor.humidity))
